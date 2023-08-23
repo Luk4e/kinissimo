8c6a18545b5540f3da647d516ec86d20f1546611
Luca <consalvi.luca@gmail.com>
2023-08-23 23:13:22 +0200

    add go-telegram-bot-api library and set up a echo bot

--- kinissimobot.go
+++ kinissimobot.go
@@ -1,81 +1,43 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
-	"net/url"
 	"os"
-	"strconv"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
-}
-
-type Update struct {
-	UpdateId int     `json:"update_id"`
-	Message  Message `json:"message"`
-}
-
-type Message struct {
-	Text string `json:"text"`
-	Chat Chat   `json:"chat"`
-}
-
-type Chat struct {
-	Id int `json:"id"`
-}
-
-func parserTelegramRequest(r *http.Request) (*Update, error) {
-	var update Update
-	if err := json.NewDecoder(r.Body).Decode(&update); err != nil {
-		log.Printf("Could not decode incoming update %s", err.Error())
-		return nil, err
+	// Retrieve the Telegram bot token from an environment variable.
+	botToken := os.Getenv("TELEGRAM_BOT_TOKEN")
+	if botToken == "" {
+		log.Fatal("TELEGRAM_BOT_TOKEN not set")
 	}
-	return &update, nil
-}
-
-func HandleTelegramWebHook(w http.ResponseWriter, r *http.Request) {
-
-	var update, err = parserTelegramRequest(r)
+	// Create a new bot instance.
+	bot, err := tgbotapi.NewBotAPI(botToken)
 	if err != nil {
-		log.Printf("Error parsing update, %s", err.Error())
-		return
+		log.Fatal(err)
 	}
 
-	var telegramResponseBody, errTelegram = sendTextToTelegramChat(update.Message.Chat.Id, update.Message.Text)
-	if errTelegram != nil {
-		log.Printf("Got error %s from telegram, reponse body is %s", errTelegram.Error(), telegramResponseBody)
-	} else {
-		log.Printf("Message text %s successfuly distributed to chat id %d", update.Message.Text, update.Message.Chat.Id)
+	// Set bot options.
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	// Set up an update configuration.
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	// Get updates from the bot.
+	updates, err := bot.GetUpdatesChan(u)
+	// Process incoming updates.
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		// Echo the received message.
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		bot.Send(msg)
 	}
-}
-
-func sendTextToTelegramChat(chatId int, text string) (string, error) {
-	log.Printf("Sending %s to chat_id: %d", text, chatId)
-	var telegramApi string = "https://api.telegram.org/bot" + os.Getenv("TELEGRAM_BOT_TOKEN") + "/sendMessage"
-	response, err := http.PostForm(
-		telegramApi,
-		url.Values{
-			"chat_id": {strconv.Itoa(chatId)},
-			"text":    {text},
-		})
-	if err != nil {
-		log.Printf("Error when posting text to the chat: %s", err.Error())
-		return "", err
-	}
-	defer response.Body.Close()
-
-	var bodyBytes, errRead = ioutil.ReadAll(response.Body)
-	if errRead != nil {
-		log.Printf("Error parsing telegram answare %s", errRead.Error())
-	}
-
-	bodyString := string(bodyBytes)
-	log.Printf("Body of telegram response: %s", bodyString)
-
-	return bodyString, nil
 }
